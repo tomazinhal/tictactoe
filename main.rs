b5671c1f96e766f972875e47113a96404d834823
tomaz <tomazinhal@gmail.com>
2021-01-10 21:50:16 +0000

    Try using a Vec
    Same error occurs - expected value, got struct 'Cell' instead

--- main.rs
+++ main.rs
@@ -1,3 +1,7 @@
+const rows: usize = 3;
+const cols: usize = 3;
+
+
 enum State {
     Empty,
     X,
@@ -10,13 +14,39 @@ struct Cell {
     y: u8
 }
 
-impl create {
-    State::Empty,
-    x: 0,
-    y: 0,
+impl Cell {
+    fn init(x: u8, y: u8) -> &Cell {
+        Cell {
+            state: State::Empty,
+            x: x,
+            y: y
+        }
+    }
+}
+
+fn is_gameover(grid: [[Cell; cols]; rows]) -> String {
+    return '1'.to_string();
 }
 
 fn main () {
-    
+    let mut grid: &[[Cell; cols]; rows];
+
+    for y in 0..rows {
+        for x in 0..cols {
+            grid[y][x]: Cell = Cell.new(x, y);
+        }
+    }
+    for (x, row) in grid.iter_mut().enumerate() {
+        for (y, cell) in row.iter_mut().enumerate() {
+            cell: Cell = Cell.new(x, y);
+        }
+    }
+    let mut cells: Vec<Cell>;
+
+    for y in 0..rows {
+        for x in 0..cols {
+            cells.push(Cell.new(x, y));
+        }
+    }
 }
 
